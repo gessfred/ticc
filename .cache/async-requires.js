6347a7bcc7d7e614d1c48602161c0333b37c5e47
Frederic Gessler <frederic.gessler@epfl.ch>
2018-08-05 09:46:02 +0200

    updated all files

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -2,13 +2,13 @@
 const preferDefault = m => m && m.default || m
 
 exports.components = {
-  "component---cache-dev-404-page-js": require("gatsby-module-loader?name=component---cache-dev-404-page-js!/Users/fredericgessler/Documents/tick/.cache/dev-404-page.js"),
-  "component---src-pages-index-js": require("gatsby-module-loader?name=component---src-pages-index-js!/Users/fredericgessler/Documents/tick/src/pages/index.js")
+  "component---cache-dev-404-page-js": require("gatsby-module-loader?name=component---cache-dev-404-page-js!/Users/fredericgessler/Documents/frontend/ticc/.cache/dev-404-page.js"),
+  "component---src-pages-index-js": require("gatsby-module-loader?name=component---src-pages-index-js!/Users/fredericgessler/Documents/frontend/ticc/src/pages/index.js")
 }
 
 exports.json = {
-  "dev-404-page.json": require("gatsby-module-loader?name=path---dev-404-page!/Users/fredericgessler/Documents/tick/.cache/json/dev-404-page.json"),
-  "index.json": require("gatsby-module-loader?name=path---index!/Users/fredericgessler/Documents/tick/.cache/json/index.json")
+  "dev-404-page.json": require("gatsby-module-loader?name=path---dev-404-page!/Users/fredericgessler/Documents/frontend/ticc/.cache/json/dev-404-page.json"),
+  "index.json": require("gatsby-module-loader?name=path---index!/Users/fredericgessler/Documents/frontend/ticc/.cache/json/index.json")
 }
 
 exports.layouts = {
--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -7,11 +7,11 @@ exports.layouts = {
 }
 
 exports.components = {
-  "component---cache-dev-404-page-js": preferDefault(require("/Users/fredericgessler/Documents/tick/.cache/dev-404-page.js")),
-  "component---src-pages-index-js": preferDefault(require("/Users/fredericgessler/Documents/tick/src/pages/index.js"))
+  "component---cache-dev-404-page-js": preferDefault(require("/Users/fredericgessler/Documents/frontend/ticc/.cache/dev-404-page.js")),
+  "component---src-pages-index-js": preferDefault(require("/Users/fredericgessler/Documents/frontend/ticc/src/pages/index.js"))
 }
 
 exports.json = {
-  "dev-404-page.json": require("/Users/fredericgessler/Documents/tick/.cache/json/dev-404-page.json"),
-  "index.json": require("/Users/fredericgessler/Documents/tick/.cache/json/index.json")
+  "dev-404-page.json": require("/Users/fredericgessler/Documents/frontend/ticc/.cache/json/dev-404-page.json"),
+  "index.json": require("/Users/fredericgessler/Documents/frontend/ticc/.cache/json/index.json")
 }
